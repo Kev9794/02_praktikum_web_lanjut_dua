98730ec2aa9ab6946d5a34996192e7deebf06e9b
Kev9794 <k.natanael88@gmail.com>
2022-02-21 15:53:25 +0700

    Langkah 2

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        echo "Selamat Datang";
+    }
+
+    public function about(){
+        echo "2041720091/Kevin Natanael Wijaya/2B";
+    }
+
+    public function articles($id){
+        return "Ini merupakan halaman artikel dengan id " . $id;
+    }
+
 }
